f524657f07f1601ab63966085958565285585585
Siim Koppel <siimk999@gmail.com>
2023-06-06 12:01:13 +0300

    p6:route-transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,90 @@
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  // Change background to blue for 1 second when the route is changed
+  animations: [
+    trigger('routeAnim', [
+      // increment - tab value is increasing
+      transition(':increment', [
+        style({
+          position: 'relative'
+        }),
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%'
+          })
+        ], { optional: true }),
+
+        group([
+          query(':leave', [
+            animate('200ms ease-in', style({
+              opacity: 0,
+              transform: 'translateX(-50px)'
+            }))
+          ], { optional: true }),
+          query(':enter', [
+            style({
+              transform: 'translateX(50px)',
+              opacity: 0
+            }),
+            animate('250ms 120ms ease-out', style({
+              opacity: 1,
+              transform: 'translateX(0)'
+            }))
+          ], { optional: true })
+        ])
+      ]),
+      // decrement - tab value decreasing
+      transition(':decrement', [
+        style({
+          position: 'relative'
+        }),
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%'
+          })
+        ], { optional: true }),
+
+        group([
+          query(':leave', [
+            animate('200ms ease-in', style({
+              opacity: 0,
+              transform: 'translateX(50px)'
+            }))
+          ], { optional: true }),
+          query(':enter', [
+            style({
+              transform: 'translateX(-50px)',
+              opacity: 0
+            }),
+            animate('250ms 120ms ease-out', style({
+              opacity: 1,
+              transform: 'translateX(0)'
+            }))
+          ], { optional: true })
+        ])
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'personal-dashboard-app';
+
+  prepareRoute(outlet: RouterOutlet) {
+    if (outlet.isActivated) return outlet.activatedRouteData['tab'];
+    // Default return value in case the condition is not met
+    return null;
+  }
 }
