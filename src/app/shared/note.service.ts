8e03c5d64e7d84f7bc4df082080921726abf88e5
Siim Koppel <siimk999@gmail.com>
2023-06-08 12:46:57 +0300

    p8,p9: note service, note cards

---
+++ src/app/shared/note.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Note } from './note.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoteService {
+
+  notes: Note[] =[
+    new Note('Test title','Test content'),
+    new Note ('Title number two', 'Hello there buddy')
+  ]
+  constructor() { }
+
+  getNotes() {
+    return this.notes
+  }
+  // return true if n.id equals the id passed into this method
+  getNote(id: string) {
+    return this.notes?.find(n => n.id == id)
+  }
+  addNote(note: Note) {
+    this.notes.push(note)
+  }
+  updateNote(id: string, updatedFields: Partial<Note>) {
+    const note = this.getNote(id)
+    // if note exists, code fails without this check in strict mode
+    if (note) {
+      Object.assign(note, updatedFields)
+    }
+  }
+
+  deleteNote(id: string) {
+    const noteIndex = this.notes.findIndex(n => n.id === id)
+    if (noteIndex == -1) return
+    this.notes.splice(noteIndex, 1)
+  }
+
+}
