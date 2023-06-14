d2e25ecd974ab359a94a820ad5306994e7438e42
Siim Koppel <siimk999@gmail.com>
2023-06-14 12:53:03 +0300

    p13,14: todo service, todo items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AddNoteComponent } from './add-note/add-note.component';
 import { NoteCardComponent } from './note-card/note-card.component';
 import { FormsModule } from '@angular/forms';
 import { EditNoteComponent } from './edit-note/edit-note.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +23,8 @@ import { EditNoteComponent } from './edit-note/edit-note.component';
     BookmarkTileComponent,
     AddNoteComponent,
     NoteCardComponent,
-    EditNoteComponent
+    EditNoteComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
